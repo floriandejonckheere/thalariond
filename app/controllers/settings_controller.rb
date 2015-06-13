3001fada51c593e0e0e4838d5d1f511629c4b7ea
Florian Dejonckheere <florian@floriandejonckheere.be>
2015-06-13 22:35:59 +0200

    Added home info panel

---
+++ app/controllers/settings_controller.rb
@@ -0,0 +1,8 @@
+class SettingsController < ApplicationController
+  before_filter :authenticate_user!
+
+  layout "panel"
+
+  def index
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,14 +16,15 @@ class User < ActiveRecord::Base
   has_many :assignments, -> { uniq }, :dependent => :destroy
   has_many :roles, -> { uniq }, :through => :assignments
 
-  def has_role?(role_sym)
-    roles.any? { |r| r.name.underscore.to_sym == role_sym.downcase }
-  end
-
   # Groups
   has_many :memberships, -> { uniq }, :dependent => :destroy
   has_many :groups, -> { uniq }, :through => :memberships
 
   has_many :ownerships, class_name: 'Membership'
   has_many :owned_groups, through: :ownerships, source: :group
+
+  ## Methods
+  def has_role?(role_sym)
+    roles.any? { |r| r.name.underscore.to_sym == role_sym.downcase }
+  end
 end
