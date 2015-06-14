a642aa364af4f2b6b467d52a4e8b63dff80f08f3
Florian Dejonckheere <florian@floriandejonckheere.be>
2015-06-14 18:11:47 +0200

    Added user list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
 
+  load_and_authorize_resource
+
   layout "panel"
 
   def index
   end
+
+  def show
+    p "WUT LOL #{params[:id]}"
+    @user = User.find(params[:id])
+  end
+
 end
