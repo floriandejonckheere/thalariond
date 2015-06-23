563e3920513141fc6a19258448d2ce712f4b1e10
Florian Dejonckheere <florian@floriandejonckheere.be>
2015-06-23 08:01:50 +0000

    Added Docker runtime files

--- start.sh
+++ start.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-rake db:migrate RAILS_ENV=production
+cd /app
+export RAILS_ENV=production
+
+# Migrate database
+rake db:migrate
+
+# Compile assets
+rake assets:precompile
+
+# Run application
+./lib/daemons/ldapd_ctl start
 rails server -b 0.0.0.0 -e production
