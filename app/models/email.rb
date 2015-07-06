c15d4f6a09ca5a590ebb0481c599ff69ae8033f9
Florian Dejonckheere <florian@floriandejonckheere.be>
2015-07-06 10:40:15 +0200

    Added mail group assigment

--- app/models/email.rb
+++ app/models/email.rb
@@ -12,6 +12,10 @@ class Email < ActiveRecord::Base
     email = self.mail + '@' + self.domain.domain
     group = Group.create!(:name => email,
                           :display_name => 'Email account for ' + email)
+    # Grant all 'mail' services access
+    Role.find_by(name: 'mail').services.each do |s|
+      s.groups << group
+    end
   end
 
   def to_ldap
