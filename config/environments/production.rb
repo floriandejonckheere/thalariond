24ccaacb39dd42e806ec68a7736b546db6bdb2b1
Florian Dejonckheere <florian@floriandejonckheere.be>
2015-07-06 08:30:24 +0000

    Edited docker configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,9 +60,20 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
+  # Mailer configuration
+  config.action_mailer.delivery_method = smtp
+  config.action_mailer.smtp_settings = {
+    :address => ENV['MAILER_HOST'],
+    :port => ENV['MAILER_PORT'],
+    :domain => ENV['MAILER_DOMAIN'],
+    :user_name => ENV['MAILER_USERNAME'],
+    :password => ENV['MAILER_PASSWORD'],
+    :authentication => ENV['MAILER_AUTHENTICATION'],
+    :enable_starttls_auto => ENV['MAILER_STARTTLS']
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
