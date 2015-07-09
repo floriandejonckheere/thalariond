63938a03de8fafa8cd60f02e7924a974b3b355d6
Florian Dejonckheere <florian@floriandejonckheere.be>
2015-07-09 19:48:00 +0200

    Updated SMTP config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,8 @@ Rails.application.configure do
   # config.action_controller.asset_host = 'http://assets.example.com'
 
   # Mailer configuration
-  config.action_mailer.delivery_method = smtp
+  config.action_mailer.default_url_options = { :host => 'thalarion.be' }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => ENV['MAILER_HOST'],
     :port => ENV['MAILER_PORT'],
