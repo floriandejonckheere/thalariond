6b26f676f9f37cb43f16c9754d4bfc986ff657cc
Florian Dejonckheere <florian@floriandejonckheere.be>
2015-11-09 15:09:53 +0100

    Use seeds for updating deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,30 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', city: cities.first)
 
-administrator = Role.create!(:name => 'administrator',
-                              :display_name => 'System administrator',
-                              :order => 4)
-Role.create!(:name => 'master',
-              :display_name => 'Master',
-              :order => 3)
-Role.create!(:name => 'operator',
-              :display_name => 'Operator',
-              :order => 2)
-Role.create!(:name => 'user',
-              :display_name => 'User',
-              :order => 1)
-Role.create!(:name => 'service',
-              :display_name => 'Service')
-Role.create!(:name => 'mail',
-              :display_name => 'Mail')
+administrator = Role.find_or_initialize_by(:name => 'administrator')
+administrator.display_name = 'System Administrator'
+administrator.order = 4
+administrator.save!
+
+master = Role.find_or_initialize_by(:name => 'master')
+master.display_name = 'Master'
+master.order = 3
+master.save!
+
+operator = Role.find_or_initialize_by(:name => 'operator')
+operator.display_name = 'Operator'
+operator.order = 2
+operator.save!
+
+user = Role.find_or_initialize_by(:name => 'user')
+user.display_name = 'User'
+user.order = 1
+user.save!
+
+service = Role.find_or_initialize_by(:name => 'service')
+service.display_name = 'Service'
+service.save!
+
+mail = Role.find_or_initialize_by(:name => 'mail')
+mail.display_name = 'Mail'
+mail.save!
