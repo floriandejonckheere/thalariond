eb8b3d9321b51c7d4ec93746a4d10d889c7a2980
Florian Dejonckheere <florian@floriandejonckheere.be>
2016-01-23 18:08:31 +0100

    Add missing bootstrap const

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,7 +23,8 @@ module ApplicationHelper
       :success => 'alert-success',
       :error => 'alert-danger',
       :alert => 'alert-warning',
-      :notice => 'alert-info'
+      :notice => 'alert-info',
+      :info => 'alert-info'
     }[flash_type.to_sym] || flash_type.to_s
   end
 end
