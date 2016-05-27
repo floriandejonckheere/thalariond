6549232727f0d832c6d8afb8d7ec95029183fc11
Florian Dejonckheere <florian@floriandejonckheere.be>
2016-05-27 18:21:34 +0200

    Add role operator overloading

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,4 +3,41 @@ class Role < ActiveRecord::Base
 
   has_and_belongs_to_many :users, :unique => true
   has_and_belongs_to_many :services, :unique => true
+
+  def <(role)
+    return nil unless self.order and role.order
+    return self.order < role.order
+  end
+
+  def >(role)
+    return nil unless self.order and role.order
+    return self.order > role.order
+  end
+
+  def <=(role)
+    return nil unless self.order and role.order
+    return self.order <= role.order
+  end
+
+  def >=(role)
+    return nil unless self.order and role.order
+    return self.order >= role.order
+  end
+
+  def ==(role)
+    return nil unless self.order and role.order
+    return self.order == role.order
+  end
+
+  def !=(role)
+    return nil unless self.order and role.order
+    return self.order != role.order
+  end
+
+  def <=>(role)
+    return -1 if self < role
+    return 0 if self == role
+    return 1 if self > role
+    return nil
+  end
 end
--- test/models/role_test.rb
+++ test/models/role_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class RoleTest < ActiveSupport::TestCase
-
   test 'role_assignment' do
     assert u('user1').has_role? :user
     assert_not u('user1').has_role? :operator
@@ -48,4 +47,227 @@ class RoleTest < ActiveSupport::TestCase
     assert_not u('admin').has_role? :administrator
   end
 
+  test 'smaller_than' do
+    assert_nil r('user') < r('mail')
+    assert_not r('user') < r('user')
+    assert r('user') < r('operator')
+    assert r('user') < r('master')
+    assert r('user') < r('administrator')
+
+    assert_nil r('operator') < r('mail')
+    assert_not r('operator') < r('user')
+    assert_not r('operator') < r('operator')
+    assert r('operator') < r('master')
+    assert r('operator') < r('administrator')
+
+    assert_nil r('master') < r('mail')
+    assert_not r('master') < r('user')
+    assert_not r('master') < r('operator')
+    assert_not r('master') < r('master')
+    assert r('master') < r('administrator')
+
+    assert_nil r('administrator') < r('mail')
+    assert_not r('administrator') < r('user')
+    assert_not r('administrator') < r('operator')
+    assert_not r('administrator') < r('master')
+    assert_not r('administrator') < r('administrator')
+
+    assert_nil r('mail') < r('mail')
+    assert_nil r('mail') < r('user')
+    assert_nil r('mail') < r('operator')
+    assert_nil r('mail') < r('master')
+    assert_nil r('mail') < r('administrator')
+  end
+
+  test 'greater_than' do
+    assert_nil r('user') > r('mail')
+    assert_not r('user') > r('user')
+    assert_not r('user') > r('operator')
+    assert_not r('user') > r('master')
+    assert_not r('user') > r('administrator')
+
+    assert_nil r('operator') > r('mail')
+    assert r('operator') > r('user')
+    assert_not r('operator') > r('operator')
+    assert_not r('operator') > r('master')
+    assert_not r('operator') > r('administrator')
+
+    assert_nil r('master') > r('mail')
+    assert r('master') > r('user')
+    assert r('master') > r('operator')
+    assert_not r('master') > r('master')
+    assert_not r('master') > r('administrator')
+
+    assert_nil r('administrator') > r('mail')
+    assert r('administrator') > r('user')
+    assert r('administrator') > r('operator')
+    assert r('administrator') > r('master')
+    assert_not r('administrator') > r('administrator')
+
+    assert_nil r('mail') > r('mail')
+    assert_nil r('mail') > r('user')
+    assert_nil r('mail') > r('operator')
+    assert_nil r('mail') > r('master')
+    assert_nil r('mail') > r('administrator')
+  end
+
+  test 'smaller_than_or_equal' do
+    assert_nil r('user') <= r('mail')
+    assert r('user') <= r('user')
+    assert r('user') <= r('operator')
+    assert r('user') <= r('master')
+    assert r('user') <= r('administrator')
+
+    assert_nil r('operator') <= r('mail')
+    assert_not r('operator') <= r('user')
+    assert r('operator') <= r('operator')
+    assert r('operator') <= r('master')
+    assert r('operator') <= r('administrator')
+
+    assert_nil r('master') <= r('mail')
+    assert_not r('master') <= r('user')
+    assert_not r('master') <= r('operator')
+    assert r('master') <= r('master')
+    assert r('master') <= r('administrator')
+
+    assert_nil r('administrator') <= r('mail')
+    assert_not r('administrator') <= r('user')
+    assert_not r('administrator') <= r('operator')
+    assert_not r('administrator') <= r('master')
+    assert r('administrator') <= r('administrator')
+
+    assert_nil r('mail') <= r('mail')
+    assert_nil r('mail') <= r('user')
+    assert_nil r('mail') <= r('operator')
+    assert_nil r('mail') <= r('master')
+    assert_nil r('mail') <= r('administrator')
+  end
+
+  test 'greater_than_or_equal' do
+    assert_nil r('user') >= r('mail')
+    assert r('user') >= r('user')
+    assert_not r('user') >= r('operator')
+    assert_not r('user') >= r('master')
+    assert_not r('user') >= r('administrator')
+
+    assert_nil r('operator') >= r('mail')
+    assert r('operator') >= r('user')
+    assert r('operator') >= r('operator')
+    assert_not r('operator') >= r('master')
+    assert_not r('operator') >= r('administrator')
+
+    assert_nil r('master') >= r('mail')
+    assert r('master') >= r('user')
+    assert r('master') >= r('operator')
+    assert r('master') >= r('master')
+    assert_not r('master') >= r('administrator')
+
+    assert_nil r('administrator') >= r('mail')
+    assert r('administrator') >= r('user')
+    assert r('administrator') >= r('operator')
+    assert r('administrator') >= r('master')
+    assert r('administrator') >= r('administrator')
+
+    assert_nil r('mail') >= r('mail')
+    assert_nil r('mail') >= r('user')
+    assert_nil r('mail') >= r('operator')
+    assert_nil r('mail') >= r('master')
+    assert_nil r('mail') >= r('administrator')
+  end
+
+  test 'equal_to' do
+    assert_nil r('user') == r('mail')
+    assert r('user') == r('user')
+    assert_not r('user') == r('operator')
+    assert_not r('user') == r('master')
+    assert_not r('user') == r('administrator')
+
+    assert_nil r('operator') == r('mail')
+    assert_not r('operator') == r('user')
+    assert r('operator') == r('operator')
+    assert_not r('operator') == r('master')
+    assert_not r('operator') == r('administrator')
+
+    assert_nil r('master') == r('mail')
+    assert_not r('master') == r('user')
+    assert_not r('master') == r('operator')
+    assert r('master') == r('master')
+    assert_not r('master') == r('administrator')
+
+    assert_nil r('administrator') == r('mail')
+    assert_not r('administrator') == r('user')
+    assert_not r('administrator') == r('operator')
+    assert_not r('administrator') == r('master')
+    assert r('administrator') == r('administrator')
+
+    assert_nil r('mail') == r('mail')
+    assert_nil r('mail') == r('user')
+    assert_nil r('mail') == r('operator')
+    assert_nil r('mail') == r('master')
+    assert_nil r('mail') == r('administrator')
+  end
+
+  test 'not_equal_to' do
+    assert_nil r('user') != r('mail')
+    assert_not r('user') != r('user')
+    assert r('user') != r('operator')
+    assert r('user') != r('master')
+    assert r('user') != r('administrator')
+
+    assert_nil r('operator') != r('mail')
+    assert r('operator') != r('user')
+    assert_not r('operator') != r('operator')
+    assert r('operator') != r('master')
+    assert r('operator') != r('administrator')
+
+    assert_nil r('master') != r('mail')
+    assert r('master') != r('user')
+    assert r('master') != r('operator')
+    assert_not r('master') != r('master')
+    assert r('master') != r('administrator')
+
+    assert_nil r('administrator') != r('mail')
+    assert r('administrator') != r('user')
+    assert r('administrator') != r('operator')
+    assert r('administrator') != r('master')
+    assert_not r('administrator') != r('administrator')
+
+    assert_nil r('mail') != r('mail')
+    assert_nil r('mail') != r('user')
+    assert_nil r('mail') != r('operator')
+    assert_nil r('mail') != r('master')
+    assert_nil r('mail') != r('administrator')
+  end
+
+  test 'spaceship' do
+    assert_equal nil, r('user') <=> r('mail')
+    assert_equal 0, r('user') <=> r('user')
+    assert_equal -1, r('user') <=> r('operator')
+    assert_equal -1, r('user') <=> r('master')
+    assert_equal -1, r('user') <=> r('administrator')
+
+    assert_equal nil, r('operator') <=> r('mail')
+    assert_equal 1, r('operator') <=> r('user')
+    assert_equal 0, r('operator') <=> r('operator')
+    assert_equal -1, r('operator') <=> r('master')
+    assert_equal -1, r('operator') <=> r('administrator')
+
+    assert_equal nil, r('master') <=> r('mail')
+    assert_equal 1, r('master') <=> r('user')
+    assert_equal 1, r('master') <=> r('operator')
+    assert_equal 0, r('master') <=> r('master')
+    assert_equal -1, r('master') <=> r('administrator')
+
+    assert_equal nil, r('administrator') <=> r('mail')
+    assert_equal 1, r('administrator') <=> r('user')
+    assert_equal 1, r('administrator') <=> r('operator')
+    assert_equal 1, r('administrator') <=> r('master')
+    assert_equal 0, r('administrator') <=> r('administrator')
+
+    assert_equal nil, r('mail') <=> r('mail')
+    assert_equal nil, r('mail') <=> r('user')
+    assert_equal nil, r('mail') <=> r('operator')
+    assert_equal nil, r('mail') <=> r('master')
+    assert_equal nil, r('mail') <=> r('administrator')
+  end
 end
