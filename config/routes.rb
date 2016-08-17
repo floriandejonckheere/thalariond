3eba1968688db99c4a93e830a94c5b98f44a581f
Florian Dejonckheere <florian@floriandejonckheere.be>
2016-08-17 11:26:05 +0200

    Remove extra Doorkeeper route init

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  use_doorkeeper :scope => 'oauth2'
-
   root :to => redirect('/home')
 
   ##
