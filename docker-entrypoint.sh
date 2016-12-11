bd1276f1c4289dfe2f8b17c262215ede0c2f62f8
Florian Dejonckheere <florian@floriandejonckheere.be>
2016-12-11 11:45:54 +0100

    Remove stale lock files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,5 +5,7 @@
 
 cd /app
 
+rm -f /app/tmp/pids/server.pid
+
 [[ ${SKIP_MIGRATE} ]] || bundle exec rake db:migrate            # Migrate relational data
 bundle exec rails server -p 8080        # Start puma
