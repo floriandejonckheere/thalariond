bb699e969ece3da478c06ce954f4219332195d99
Florian Dejonckheere <florian@floriandejonckheere.be>
2019-01-25 21:47:59 +0100

    Remove paper trail integration from AppController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery :with => :exception
 
   # Check if authorization is performed for every controller action
   check_authorization :unless => :devise_controller?
@@ -11,17 +13,6 @@ class ApplicationController < ActionController::Base
     resource = controller_name.singularize.to_sym
     method = "#{resource}_params"
     params[resource] &&= send(method) if respond_to?(method, true)
-  end
-
-  # Allow Paper Trail to use current_user
-  before_action :set_paper_trail_whodunnit
-
-  # Extra metadata for Paper Trail
-  def info_for_paper_trail
-    {
-      :ip => request.remote_ip,
-      :user_agent => request.user_agent
-    }
   end
 
   # CanCanCan Access Denied
